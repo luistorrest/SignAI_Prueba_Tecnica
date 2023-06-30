126e07e0b264feefa1fc3b878505a81011d8fa92
luistorrest <70786070+luistorrest@users.noreply.github.com>
2023-06-29 22:16:00 -0500

    first interface model

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2OTE3Ue-s2a-oIiLBIZXkM_rgR9HjfPE",
+  authDomain: "signai-prueba-tecnica.firebaseapp.com",
+  projectId: "signai-prueba-tecnica",
+  storageBucket: "signai-prueba-tecnica.appspot.com",
+  messagingSenderId: "874509627521",
+  appId: "1:874509627521:web:f8c3e589c2c0fb97f3dd80"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
